6ea5a7d2ef4d37812dfa71e1b49000f84eb747fe
anveshbathini <itsanvesh@gmail.com>
2016-08-11 15:42:36 +0530

    aa

---
+++ ProductBidApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductBidApp.MvcApplication" Language="C#" %>
---
+++ ProductBidApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductBidApp.Startup))]
+namespace ProductBidApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
